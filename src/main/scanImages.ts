114fa757662f483a3492ffaeebc0d050a5752d5d
veryspry <ehlinger.matt@gmail.com>
2020-09-14 11:08:38 -0500

    refactor: split things out into more modules

---
+++ src/main/scanImages.ts
@@ -0,0 +1,81 @@
+import fs from 'fs-extra';
+import { SiteImageData } from '../types';
+import {
+	saveImageDataToDisk,
+	getImageFilePaths,
+	hasImageBeenCompressed,
+	getFileHash,
+} from './utils';
+
+/**
+ * Scans a site's wp-content dir for images
+ * Currently only supports jpeg
+ *
+ * @param siteID
+ *
+ * @returns ImageData[]
+ */
+export function scanImagesFactory(serviceContainer, imageDataStore) {
+	return async function(siteID: string): Promise<SiteImageData> {
+		const site = serviceContainer.siteData.getSite(siteID);
+
+		if (!site) {
+			/**
+			 * @todo improve this error handling?
+			 */
+			return new Promise((resolve, reject) => reject(new Error('Site not found!')));
+		}
+
+		const filePaths = await getImageFilePaths(site);
+
+		const filesWithHashes = await Promise.all(
+			filePaths.map((file: string) => getFileHash(file)),
+		);
+
+		const existingImageData = imageDataStore[siteID]?.imageData || {};
+
+		let totalImagesSize = 0;
+
+		const updatedSiteImageData = await filePaths.reduce(async (
+			imageData: Promise<SiteImageData['imageData']>,
+			filePath: string,
+		) => {
+			const fileSize = fs.statSync(filePath).size;
+			totalImagesSize += fileSize;
+
+			const fileHash = await getFileHash(filePath);
+
+			if (hasImageBeenCompressed(fileHash, existingImageData)) {
+				return imageData;
+			}
+
+			return {
+				...(await imageData),
+				[fileHash]: {
+					originalImageHash: fileHash,
+					filePath,
+					originalSize: fileSize,
+				},
+			};
+		}, Promise.resolve(existingImageData)) as SiteImageData['imageData'];
+
+
+		/**
+		 * @todo read from backup dir and rectify any copied image data
+		 */
+		const currentSiteImageData = {
+			...(imageDataStore[siteID] || {}),
+			imageData: updatedSiteImageData,
+			lastScan: new Date(),
+			originalTotalSize: totalImagesSize,
+			compressedTotalSize: undefined,
+			imageCount: filesWithHashes.length,
+		};
+
+		imageDataStore[siteID] = currentSiteImageData;
+
+		saveImageDataToDisk(siteID, currentSiteImageData, serviceContainer);
+
+		return currentSiteImageData;
+	}
+};
