e93c4d569a83ea7420c07a1c0534e681aaac143c
Tyler Halstead <tylerhhal@gmail.com>
2020-09-17 12:14:48 -0500

    feat(PRO-515): create initial structure for render thread
    feat(PRO-515): full structure for renderer thread

---
+++ src/renderer/fileListView.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// https://getflywheel.github.io/local-addon-api/modules/_local_renderer_.html
+import * as LocalRenderer from '@getflywheel/local/renderer';
+
+// https://github.com/getflywheel/local-components
+import { Button, FlyModal, Title, Text } from '@getflywheel/local-components';
+
+export const FileListView = () =>  (
+	<div>
+		Hello World, this is the file list view
+	</div>
+);
