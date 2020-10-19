c6041010201954547d8e6130eb05b38f661d839a
Andrew Botz <andrew.botz@getflywheel.com>
2020-10-19 17:03:31 -0500

    feat(LOC-2184): Add reporting to image optimizer

--- src/main/scanImages.ts
+++ src/main/scanImages.ts
@@ -3,7 +3,7 @@ import * as LocalMain from '@getflywheel/local/main';
 import { SiteImageData, Store } from '../types';
 import { saveImageDataToDisk } from './utils';
 import { IPC_EVENTS } from '../constants';
-
+import { reportScanRequest, reportScanSuccess, reportScanFailure } from './errorReporting';
 
 /**
  * Scans a site's wp-content dir for images
@@ -14,118 +14,125 @@ import { IPC_EVENTS } from '../constants';
  * @returns ImageData[]
  */
 export function scanImagesFactory(serviceContainer: LocalMain.ServiceContainerServices, imageDataStore: Store) {
-	return async function(siteID: string) {
-		const site = serviceContainer.siteData.getSite(siteID);
-		let siteData = imageDataStore.getStateBySiteID(siteID);
-		const imageData = siteData.imageData || {};
+	return async function (siteID: string) {
+		try {
+			reportScanRequest(siteID);
+			const site = serviceContainer.siteData.getSite(siteID);
+			let siteData = imageDataStore.getStateBySiteID(siteID);
+			const imageData = siteData.imageData || {};
 
-		if (!site) {
-			serviceContainer.sendIPCEvent(IPC_EVENTS.SCAN_IMAGES_FAILURE, new Error('Site not found!'));
-			return;
-		}
+			if (!site) {
+				serviceContainer.sendIPCEvent(IPC_EVENTS.SCAN_IMAGES_FAILURE, new Error('Site not found!'));
+				return;
+			}
 
-		const scanningSiteImageData: Partial<SiteImageData> = {
-			scanInProgress: true,
-		};
+			const scanningSiteImageData: Partial<SiteImageData> = {
+				scanInProgress: true,
+			};
 
-		imageDataStore.setStateBySiteID(siteID, scanningSiteImageData);
+			imageDataStore.setStateBySiteID(siteID, scanningSiteImageData);
 
-		saveImageDataToDisk(imageDataStore, serviceContainer);
+			saveImageDataToDisk(imageDataStore, serviceContainer);
 
-		siteData = imageDataStore.getStateBySiteID(siteID);
+			siteData = imageDataStore.getStateBySiteID(siteID);
 
-		/**
-		 * @todo - remove @ts-ignore once the new Local api changes are published
-		 */
-		// @ts-ignore
-		const childProcess = LocalMain.workerFork(
-			path.join(__dirname, 'scanImagesProcess'),
-		);
-
-		/**
-		 * @todo - remove @ts-ignore once the new Local api changes are published
-		 */
-		// @ts-ignore
-		const processMessageHelper: (name: string, payload?: any) => any = LocalMain.childProcessMessagePromiseFactory(childProcess);
-
-		/**
-		 * @todo - remove @ts-ignore once the new Local api changes are published
-		 */
-		// @ts-ignore
-		const filePaths = await processMessageHelper<string[]>(
-			'get-file-paths',
-			{
-				webRoot: site.paths.webRoot,
-			},
-		);
-
-		/**
-		 * @todo - remove @ts-ignore once the new Local api changes are published
-		 */
-		// @ts-ignore
-		const updatedImageData = await processMessageHelper<SiteImageData['imageData']>(
-			'get-image-stats',
-			{
-				filePaths,
-				imageData,
-			},
 			/**
-			 * @todo - kill the type casting once this is exposed in Local API
+			 * @todo - remove @ts-ignore once the new Local api changes are published
 			 */
-		) as SiteImageData['imageData'];
+			// @ts-ignore
+			const childProcess = LocalMain.workerFork(
+				path.join(__dirname, 'scanImagesProcess'),
+			);
 
-		const totalImagesSize = await Object.values(updatedImageData).reduce(
-			(acc, data) => {
-				return acc + data.originalSize;
-			}, 0
-		);
+			/**
+			 * @todo - remove @ts-ignore once the new Local api changes are published
+			 */
+			// @ts-ignore
+			const processMessageHelper: (name: string, payload?: any) => any = LocalMain.childProcessMessagePromiseFactory(childProcess);
 
-		const compressedTotalSize = await Object.values(updatedImageData).reduce(
-			(acc, data) => {
-				if (typeof data.compressedSize === 'number') {
-					return acc + data.compressedSize;
-				} else {
-					return acc;
-				}
-			}, 0
-		);
+			/**
+			 * @todo - remove @ts-ignore once the new Local api changes are published
+			 */
+			// @ts-ignore
+			const filePaths = await processMessageHelper<string[]>(
+				'get-file-paths',
+				{
+					webRoot: site.paths.webRoot,
+				},
+			);
 
-		const totalCompressedCount = await Object.values(updatedImageData).reduce(
-			(acc, data) => {
-				if (data.compressedImageHash) {
-					return acc + 1;
-				} else {
-					return acc;
-				}
-			}, 0
-		);
+			/**
+			 * @todo - remove @ts-ignore once the new Local api changes are published
+			 */
+			// @ts-ignore
+			const updatedImageData = await processMessageHelper<SiteImageData['imageData']>(
+				'get-image-stats',
+				{
+					filePaths,
+					imageData,
+				},
+				/**
+				 * @todo - kill the type casting once this is exposed in Local API
+				 */
+			) as SiteImageData['imageData'];
 
-		const compressedImagesOriginalSize = await Object.values(updatedImageData).reduce(
-			(acc, data) => {
-				if (data.compressedImageHash) {
+			const totalImagesSize = await Object.values(updatedImageData).reduce(
+				(acc, data) => {
 					return acc + data.originalSize;
-				} else {
-					return acc;
-				}
-			}, 0
-		);
+				}, 0
+			);
 
-		const nextSiteImageData = {
-			...siteData,
-			imageData: updatedImageData,
-			scanInProgress: false,
-			lastScan: Date.now(),
-			originalTotalSize: totalImagesSize,
-			compressedTotalSize: compressedTotalSize,
-			imageCount: filePaths.length,
-			totalCompressedCount: totalCompressedCount,
-			compressedImagesOriginalSize: compressedImagesOriginalSize,
-		};
+			const compressedTotalSize = await Object.values(updatedImageData).reduce(
+				(acc, data) => {
+					if (typeof data.compressedSize === 'number') {
+						return acc + data.compressedSize;
+					} else {
+						return acc;
+					}
+				}, 0
+			);
 
-		imageDataStore.setStateBySiteID(siteID, nextSiteImageData);
+			const totalCompressedCount = await Object.values(updatedImageData).reduce(
+				(acc, data) => {
+					if (data.compressedImageHash) {
+						return acc + 1;
+					} else {
+						return acc;
+					}
+				}, 0
+			);
 
-		saveImageDataToDisk(imageDataStore, serviceContainer);
+			const compressedImagesOriginalSize = await Object.values(updatedImageData).reduce(
+				(acc, data) => {
+					if (data.compressedImageHash) {
+						return acc + data.originalSize;
+					} else {
+						return acc;
+					}
+				}, 0
+			);
 
-		serviceContainer.sendIPCEvent(IPC_EVENTS.SCAN_IMAGES_COMPLETE, nextSiteImageData);
+			const nextSiteImageData = {
+				...siteData,
+				imageData: updatedImageData,
+				scanInProgress: false,
+				lastScan: Date.now(),
+				originalTotalSize: totalImagesSize,
+				compressedTotalSize: compressedTotalSize,
+				imageCount: filePaths.length,
+				totalCompressedCount: totalCompressedCount,
+				compressedImagesOriginalSize: compressedImagesOriginalSize,
+			};
+
+			imageDataStore.setStateBySiteID(siteID, nextSiteImageData);
+
+			saveImageDataToDisk(imageDataStore, serviceContainer);
+
+			serviceContainer.sendIPCEvent(IPC_EVENTS.SCAN_IMAGES_COMPLETE, nextSiteImageData);
+			reportScanSuccess(siteID, filePaths.length, totalCompressedCount);
+		} catch (error) {
+			reportScanFailure(siteID, error);
+			return error;
+		}
 	}
 };
