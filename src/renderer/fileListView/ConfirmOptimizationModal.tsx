945ee52618b74e2f3ad761c51265abcbcafc2cd2
veryspry <ehlinger.matt@gmail.com>
2020-10-22 15:04:39 -0500

    feat: toggle optimize confirm modal text depending on current settings

--- src/renderer/fileListView/ConfirmOptimizationModal.tsx
+++ src/renderer/fileListView/ConfirmOptimizationModal.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
 import { PrimaryButton, TextButton, Divider, Title } from '@getflywheel/local-components';
+import { Preferences } from '../../types';
 
 interface IModalProps {
 	onSubmit: () => void
 	openPreferencesModal: () => void
 	onConfirmSelect?: () => void
 	onCancelSelect?: () => void
+	preferences: Preferences;
 }
 
-export const ConfirmOptimizationModal = ( props: IModalProps ) =>  (
-	<div className='fileList_Modal'>
-		<Title size="l" container={{ margin: 'm 30 30'}}> Confirm Optimization </Title>
-		<Divider />
-			<div className='fileList_Modal_Text'>
-				Optimizing images will strip metadata and reduce image sizes to improve your site's performance.
-				<br />
-				<br />
-				<TextButton
-					className="fileList_Modal_Settings_Button"
-					onClick={props.openPreferencesModal}
-				>
-					View Settings
-				</TextButton>
-			</div>
-		<Divider />
-		<PrimaryButton
-			onClick={props.onSubmit}
-		>
-			Optimize Images
-		</PrimaryButton>
-	</div>
-);
+
+export const ConfirmOptimizationModal = (props: IModalProps) => {
+	const {
+		openPreferencesModal,
+		onSubmit,
+		preferences,
+	} = props;
+
+	let displayText = 'Optimizing images will not strip metadata and will reduce image sizes to improve your site\'s performance.';
+
+	if (preferences.stripMetaData) {
+		displayText = 'Optimizing images will strip metadata and reduce image sizes to improve your site\'s performance.';
+	}
+
+	return (
+		<div className='fileList_Modal'>
+			<Title size="l" container={{ margin: 'm 30 30'}}> Confirm Optimization </Title>
+			<Divider />
+				<div className='fileList_Modal_Text'>
+					{displayText}
+					<br />
+					<br />
+					<TextButton
+						className="fileList_Modal_Settings_Button"
+						onClick={openPreferencesModal}
+					>
+						View Settings
+					</TextButton>
+				</div>
+			<Divider />
+			<PrimaryButton
+				onClick={onSubmit}
+			>
+				Optimize Images
+			</PrimaryButton>
+		</div>
+	);
+};
