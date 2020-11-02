fddccfe416d453eba090bd6c87b712de5d7911a4
Andrew Botz <andrew.botz@getflywheel.com>
2020-11-02 16:24:34 -0600

    feat(LOC-2290): Add missing IO analytic events

--- src/renderer/analytics.ts
+++ src/renderer/analytics.ts
@@ -9,28 +9,26 @@ const EVENT_LIFECYCLES = {
 }
 
 export const ANALYTIC_EVENT_TYPES = {
-	SCAN_START: `${BASE}scan${EVENT_LIFECYCLES.START}`, // done?
-	SCAN_SUCCESS: `${BASE}scan${EVENT_LIFECYCLES.SUCCESS}`, // done?
-	SCAN_FAILURE: `${BASE}scan${EVENT_LIFECYCLES.FAILURE}`, // done?
+	SCAN_START: `${BASE}scan${EVENT_LIFECYCLES.START}`,
+	SCAN_SUCCESS: `${BASE}scan${EVENT_LIFECYCLES.SUCCESS}`,
+	SCAN_FAILURE: `${BASE}scan${EVENT_LIFECYCLES.FAILURE}`,
 	SCAN_CANCEL: `${BASE}scan${EVENT_LIFECYCLES.CANCEL}`, // do not think this is a planned feature?
-	OPTIMIZE_START: `${BASE}optimize${EVENT_LIFECYCLES.START}`, // done?
+	OPTIMIZE_START: `${BASE}optimize${EVENT_LIFECYCLES.START}`,
 	OPTIMIZE_SUCCESS: `${BASE}optimize${EVENT_LIFECYCLES.SUCCESS}`,
 	OPTIMIZE_FAILURE: `${BASE}optimize${EVENT_LIFECYCLES.FAILURE}`,
 	OPTIMIZE_CANCEL: `${BASE}optimize${EVENT_LIFECYCLES.CANCEL}`,
-	OPTIMIZE_INCLUDE_ALL_FILES: `${BASE}include_all`, // done?
-	OPTIMIZE_EXCLUDE_ALL_FILES: `${BASE}exclude_all`, // done?
-	OPTIMIZE_INCLUDE_SINGLE_FILE: `${BASE}include`, // done?
-	OPTIMIZE_EXLUDE_SINGLE_FILE: `${BASE}exclude`, // done?
-	PREFERENCE_METADATA: `${BASE}preference_metadata` // done?
+	OPTIMIZE_INCLUDE_ALL_FILES: `${BASE}include_all`,
+	OPTIMIZE_EXCLUDE_ALL_FILES: `${BASE}exclude_all`,
+	OPTIMIZE_INCLUDE_SINGLE_FILE: `${BASE}include`,
+	OPTIMIZE_EXLUDE_SINGLE_FILE: `${BASE}exclude`,
+	PREFERENCE_METADATA: `${BASE}preference_metadata`
 }
 
 export const reportAnalytics = (eventName: string, additionalProperties?: Object) => {
 	const channel = 'analyticsV2:trackEvent';
 	if (additionalProperties) {
 		ipcRenderer.send(channel, eventName, additionalProperties);
-		console.log(channel, eventName, additionalProperties);
 	} else {
 		ipcRenderer.send(channel, eventName);
-		console.log(channel, eventName);
 	}
 }
