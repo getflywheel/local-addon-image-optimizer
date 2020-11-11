637b022e866323b5ec67ed30e0b0354e9eed11ab
Jordan Hoff <jordan@getflywheel.com>
2020-11-11 15:31:59 -0600

    fix: add assertions to ensureBins test

--- src/main/ensureBins.test.ts
+++ src/main/ensureBins.test.ts
@@ -8,8 +8,8 @@ describe('Bundled binaries', () => {
 	it('includes jpeg-recompress for supported os\'s', () => {
 		const vendorDir = path.join(__dirname, '..', '..', 'vendor');
 
-		expect(fs.existsSync(path.join(vendorDir, 'darwin', 'jpeg-recompress')));
-		expect(fs.existsSync(path.join(vendorDir, 'linux', 'jpeg-recompress')));
-		expect(fs.existsSync(path.join(vendorDir, 'win32', 'jpeg-recompress.exe')));
+		expect(fs.existsSync(path.join(vendorDir, 'darwin', 'jpeg-recompress'))).toBeTrue();
+		expect(fs.existsSync(path.join(vendorDir, 'linux', 'jpeg-recompress'))).toBeTrue();
+		expect(fs.existsSync(path.join(vendorDir, 'win32', 'jpeg-recompress.exe'))).toBeTrue();
 	});
 });
