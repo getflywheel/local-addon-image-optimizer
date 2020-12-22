7f55a6a02311bb1368f1f1fec74741ea167b4dad
veryspry <ehlinger.matt@gmail.com>
2020-12-22 10:53:55 -0600

    refactor: make revert confirm modal copy uniform

--- src/renderer/confirmRestoreBackupModalContents.tsx
+++ src/renderer/confirmRestoreBackupModalContents.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { Title, PrimaryButton } from '@getflywheel/local-components';
 
 
-
 const ConfirmRestoreBackupModalContents = (props) => {
 	const { onSubmit } = props;
 
 	return (
 		<div>
-			<Title size="l" container={{ margin: 'm 30 30'}}>Restore from backup</Title>
+			<Title size="l" container={{ margin: 'm 30 30' }}>Revert to backup</Title>
 			<br />
-			Restoring an image from backup will permanantly delete the compressed image and replace it with the original image. The backup will not be deleted.
+			Reverting an image to a backup will permanantly delete the compressed image and replace it with the original image. The backup will not be deleted.
 			<br/>
 
 			<PrimaryButton
@@ -20,6 +19,6 @@ const ConfirmRestoreBackupModalContents = (props) => {
 			</PrimaryButton>
 		</div>
 	);
-}
+};
 
 export default ConfirmRestoreBackupModalContents;
